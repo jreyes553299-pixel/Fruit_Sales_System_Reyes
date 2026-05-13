0f3caa6d2d786a8ba9bc746e9378335d03454879
jules henri <jules>
2026-05-13 22:38:11 +0800

    first

---
+++ app/Http/Controllers/FruitController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\fruit;
+use Illuminate\Http\Request;
+
+class FruitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(fruit $fruit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(fruit $fruit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, fruit $fruit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(fruit $fruit)
+    {
+        //
+    }
+}
---
+++ app/Models/fruit.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class fruit extends Model
+{
+    protected $table = 'fruits';
+    protected $fillable = [
+        'name',
+        'price',
+        'stock_quantity',
+        'description',
+        'availability',
+    ];
+
+}
